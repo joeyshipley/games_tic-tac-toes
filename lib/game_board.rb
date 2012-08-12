0a06fa2532d12c363b66bba420d7f794e596b7a1
Joey Shipley <joey.shipley@gmail.com>
2012-08-12 13:37:26 -0400

    Adds computer/player choices to board display.

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,39 +1,56 @@
 class GameBoard
-  attr_accessor :moves
+  attr_accessor :tiles
 
   def initialize
-    @moves = []
+    @tiles = []
     (1..9).each do |i|
-      @moves.push({ :square => i, :owner => :none })
+      @tiles.push({ :square => i, :owner => :none })
     end
   end
 
-  def get_move_owner(square)
-    move = @moves.find { |move| move[:square] == square }
-    move[:owner]
+  def get_tile_owner(square)
+    tile = @tiles.find { |tile| tile[:square] == square }
+    tile[:owner]
   end
 
   def apply_move(owner, square)
-    move = @moves.find { |move| move[:square] == square }
-    move[:owner] = owner
+    tile = @tiles.find { |tile| tile[:square] == square }
+    tile[:owner] = owner
   end
 
   def for_display
+    square1 = get_display_value_for_tile_owner(1)
+    square2 = get_display_value_for_tile_owner(2)
+    square3 = get_display_value_for_tile_owner(3)
+    square4 = get_display_value_for_tile_owner(4)
+    square5 = get_display_value_for_tile_owner(5)
+    square6 = get_display_value_for_tile_owner(6)
+    square7 = get_display_value_for_tile_owner(7)
+    square8 = get_display_value_for_tile_owner(8)
+    square9 = get_display_value_for_tile_owner(9)
+
     board = ""
     board += "                   \n"
     board += "      |     |      \n"
-    board += "   1  |  2  |  3   \n"
+    board += "   #{square1}  |  #{square2}  |  #{square3}   \n"
     board += "      |     |      \n"
     board += " ----- ----- ----- \n"
     board += "      |     |      \n"
-    board += "   4  |  5  |  6   \n"
+    board += "   #{square4}  |  #{square5}  |  #{square6}   \n"
     board += "      |     |      \n"
     board += " ----- ----- ----- \n"
     board += "      |     |      \n"
-    board += "   7  |  8  |  9   \n"
+    board += "   #{square7}  |  #{square8}  |  #{square9}   \n"
     board += "      |     |      \n"
     board += "                   \n"
 
     return board
+  end
+
+  def get_display_value_for_tile_owner(square)
+    tile = @tiles.find { |tile| tile[:square] == square }
+    return "X" if tile[:owner] == :player
+    return "O" if tile[:owner] == :computer
+    return square if tile[:owner] == :none
   end
 end
--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -14,12 +14,30 @@ describe 'GameBoard' do
         is_match.should equal(true)
       end
     end
+
+    describe "and the player has made a move" do
+      it "displays the tile the player has chosen as the players" do
+        @board.apply_move(:player, 1)
+        pattern_match = @board.for_display =~ /X.*2.*3.*4.*5.*6.*7.*8.*9/m
+        is_match = pattern_match.nil? ? false : true
+        is_match.should equal(true)
+      end
+    end
+
+    describe "and the computer has made a move" do
+      it "displays the tile the computer has chosen as the computers" do
+        @board.apply_move(:computer, 1)
+        pattern_match = @board.for_display =~ /O.*2.*3.*4.*5.*6.*7.*8.*9/m
+        is_match = pattern_match.nil? ? false : true
+        is_match.should equal(true)
+      end
+    end
   end
 
   describe "When the players move is 1" do
     it "applies the players move to the game board" do
       @board.apply_move(:player, 1)
-      move_owner = @board.get_move_owner(1)
+      move_owner = @board.get_tile_owner(1)
       move_owner.should equal(:player)
     end
   end
@@ -27,7 +45,7 @@ describe 'GameBoard' do
   describe "When the computer move is 1" do
     it "applies the computers move to the game board" do
       @board.apply_move(:computer, 1)
-      move_owner = @board.get_move_owner(1)
+      move_owner = @board.get_tile_owner(1)
       move_owner.should equal(:computer)
     end
   end
