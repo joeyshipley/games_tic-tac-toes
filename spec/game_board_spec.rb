b464f3704bb81c20c5943996062b54557d76b3c9
Joey Shipley <joey.shipley@gmail.com>
2012-08-28 08:56:03 -0400

    Updating should equals() to be should == instead.

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -6,22 +6,20 @@ describe 'GameBoard' do
   describe "When we need the board to be displayed" do
     describe "and no moves have been made" do
       it "displays only the numeric values of the moves" do
-        pattern_match = board.for_display =~ /1.*2.*3.*4.*5.*6.*7.*8.*9/m
-        is_match = pattern_match.nil? ? false : true
-        is_match.should equal(true)
+        board.for_display.should match /1.*2.*3.*4.*5.*6.*7.*8.*9/m
       end
     end
 
     it "displays the player as an X" do
       board.apply_move(:player, "1")
       result = board.tile_display("1")
-      result.should match /X/
+      result.should == 'X'
     end
 
     it "displays the computer as an O" do
       board.apply_move(:computer, "1")
       result = board.tile_display("1")
-      result.should match /O/
+      result.should == 'O'
     end
 
     describe "and the player has made a move" do
@@ -47,7 +45,7 @@ describe 'GameBoard' do
     it "applies the players move to the game board" do
       board.apply_move(:player, "1")
       move_owner = board.get_tile_owner("1")
-      move_owner.should equal(:player)
+      move_owner.should == :player
     end
   end
 
@@ -55,7 +53,7 @@ describe 'GameBoard' do
     it "applies the computers move to the game board" do
       board.apply_move(:computer, "1")
       move_owner = board.get_tile_owner("1")
-      move_owner.should equal(:computer)
+      move_owner.should == :computer
     end
   end
 
@@ -64,21 +62,21 @@ describe 'GameBoard' do
       it "returns that it is not valid" do
         board.apply_move(:player, "1")
         result = board.is_tile_available("1")
-        result.should equal(false)
+        result.should == false
       end
     end
 
     describe "that is a letter" do
       it "returns that it is not valid" do
         result = board.is_tile_valid("A")
-        result.should equal(false)
+        result.should == false
       end
     end
 
     describe "that is out of the tile range" do
       it "returns that it is not valid" do
         result = board.is_tile_valid("0")
-        result.should equal(false)
+        result.should == false
       end
     end
   end
